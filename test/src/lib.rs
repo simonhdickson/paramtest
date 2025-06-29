243ca352c1aa0ec2a5dcae7ee128146bc3f73702
Simon Dickson <683945+simonhdickson@users.noreply.github.com>
2025-06-29 22:29:46 +0100

    Add ci

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -1,63 +1,67 @@
-use paramtest::{paramtest, tokio_paramtest};
+#[cfg(test)]
+mod tests {
 
-#[paramtest(
+    use paramtest::{paramtest, tokio_paramtest};
+
+    #[paramtest(
     one=(1, 2),
     two=(2, 3),
     three=(100, 101),
 )]
-fn add_on(input: u64, output: u64) {
-    assert_eq!(output, input + 1)
-}
+    fn add_on(input: u64, output: u64) {
+        assert_eq!(output, input + 1)
+    }
 
-#[paramtest(
+    #[paramtest(
     test1=("", true),
     test2=("hello", false),
     test3=("world", false),
 )]
-fn empty(input: &str, pass: bool) {
-    assert_eq!(pass, input.is_empty())
-}
+    fn empty(input: &str, pass: bool) {
+        assert_eq!(pass, input.is_empty())
+    }
 
-#[paramtest(
+    #[paramtest(
     test1=(None, false),
     test2=(Some("hello"), true),
     test3=(Some("world"), true),
 )]
-fn has_value(input: Option<&str>, pass: bool) {
-    assert_eq!(pass, input.is_some())
-}
+    fn has_value(input: Option<&str>, pass: bool) {
+        assert_eq!(pass, input.is_some())
+    }
 
-#[derive(PartialEq, Eq)]
-enum TestEnum {
-    Variant1,
-    Variant2,
-}
+    #[derive(PartialEq, Eq)]
+    enum TestEnum {
+        Variant1,
+        Variant2,
+    }
 
-#[paramtest(
+    #[paramtest(
     test1=(TestEnum::Variant1, false),
     test2=(TestEnum::Variant2, true),
 )]
-fn variant(input: TestEnum, pass: bool) {
-    assert_eq!(pass, input == TestEnum::Variant2)
-}
+    fn variant(input: TestEnum, pass: bool) {
+        assert_eq!(pass, input == TestEnum::Variant2)
+    }
 
-#[tokio_paramtest(
+    #[tokio_paramtest(
     test1=(TestEnum::Variant1, false),
     test2=(TestEnum::Variant2, true),
 )]
-async fn tokio_variant(input: TestEnum, pass: bool) {
-    assert_eq!(pass, input == TestEnum::Variant2);
-}
+    async fn tokio_variant(input: TestEnum, pass: bool) {
+        assert_eq!(pass, input == TestEnum::Variant2);
+    }
 
-#[tokio_paramtest(
+    #[tokio_paramtest(
     test1=(TestEnum::Variant1, false),
     test2=(TestEnum::Variant2, true),
 )]
-async fn tokio_variant_result(
-    input: TestEnum,
-    pass: bool,
-) -> Result<(), Box<dyn std::error::Error>> {
-    assert_eq!(pass, input == TestEnum::Variant2);
+    async fn tokio_variant_result(
+        input: TestEnum,
+        pass: bool,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(pass, input == TestEnum::Variant2);
 
-    Ok(())
+        Ok(())
+    }
 }
